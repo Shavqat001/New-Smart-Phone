db3b3c3a9250e9aa4f393d089c2bece36b767b10
Shavqqat001 <nazriev.shavqatjon@gmail.com>
2023-03-16 23:47:28 +0500

    add password buttons

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 let onOff = document.querySelector('.power-button');
-var bool = true;
-function changeBg() {
-    console.log('click');
-    if (bool == true) {
-        onOff.style.background = 'url(./images/screen-bg.jpg) 50% center / 100% no-repeat';
-        bool = false;
-    }
-    else {
-        onOff.style.background = '#000';
-        bool = true;
-    }
+let screen = document.querySelector('.screen');
+
+onOff.onclick = () => {
+    screen.classList.toggle('BG');
 }
