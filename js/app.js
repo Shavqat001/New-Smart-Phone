66d4e02843b7e1e4de08611e57fad25fa0c97413
Shavqqat001 <nazriev.shavqatjon@gmail.com>
2023-03-17 22:34:56 +0500

    adding functionallity to password buttons

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
-let onOff = document.querySelector('.power-button');
 let screen = document.querySelector('.screen');
+let onOff = document.querySelector('.power-button');
+let passwordBtn = document.querySelectorAll('.password-button');
+let passwordField = document.querySelector('.screen>input');
+let okButton = document.querySelector('.ok-button');
+let clearButton = document.querySelector('.clear-button');
 
 onOff.onclick = () => {
     screen.classList.toggle('BG');
+    screen.classList.toggle('unvisible');
+}
+
+for (let i = 0; i < passwordBtn.length; i++) {
+    passwordBtn[i].onclick = () => {
+        passwordField.value += passwordBtn[i].textContent;
+    }
+}
+okButton.onclick = () => {
+    if(passwordField.value == 2807) {
+        
+    } else {
+        alert('Write the password');
+    }
+}
+
+clearButton.onclick = () => {
+    passwordField.value = '';
 }
